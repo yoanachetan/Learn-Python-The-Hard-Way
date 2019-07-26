c992b93482572f84c0603e225db1884e949fe8ec
yoanachetan <yoana.chetan@gmail.com>
2019-07-26 14:11:07 +0200

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yey! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("Happy to print.")
+#Study Drills
+#The Study Drills contain things you should try to do. If you can’t, skip it and come back later. For this exercise, try these things:
+#1. Make your script print another line.
+#2. Make your script print only one of the lines.
+#3. Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
