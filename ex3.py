f99cccbb3467fcb84264e90ebde70db3a9c2f738
yoanachetan <yoana.chetan@gmail.com>
2019-07-27 12:38:14 +0200

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,30 @@
+print("I will now count my chickens") # Prints the statement in the  "" (string)
+
+print("Hens", 25 + 30 / 6) # Prints a string and a mathematical operation (25 + 5 = 30)
+print("Roosters", 100 - 25 * 3 % 4 ) # Prints the string "Roosters" and a mathematical operation (3 * 25 = 75 % 4 = 3 >>> 100 - 3 = 97)
+
+print("Now I will count the eggs:") # Prints the string
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # Print the result of the eggs calculation
+
+print("It is true that 3 + 2 < 5 - 7?")# Print the string
+
+print(3 + 2 < 5 - 7)# Print the answer
+
+print("What is 3 + 2?", 3 + 2) # Print the string and calculate the answer
+print("What is 5 - 7", 5 - 7) # Print the string and calculate the answer
+
+print("Oh, that's why it's False.") # Print the string
+
+print("How about some more.") # Print the string
+
+print("Is it greater?", 5 > -2) # Print the string and calculate and print 5 > - 2
+print("Is it greater or equal?", 5 >= -2) # Print the string and calculate 5 > - 2
+print("It is less or equal?", 5 <= -2) # Print the string and calculate 5 < = - 2
+
+Study Drills
+#1. Above each line,use the #to write a comment to yourself explaining what the line does.
+# done
+#2. Remember in Exercise 0 when you started python3.6? Start python 3.6 this way again and,using the math operators, use Python as a calculator.
+#3. Find something you need to calculate and write a new .py file that does it.
+#4. Rewrite ex3.py to use floating point numbers so it’s more accurate. 20.0isfloating point.
