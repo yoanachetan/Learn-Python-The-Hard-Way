279efc3ce5b800b1b0883c4a5cfefc0214b4c17d
yoanachetan <yoana.chetan@gmail.com>
2019-07-28 18:40:49 +0200

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,24 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+Study Drills
+#1. Go through this program and write a comment above each line explaining it. 
+#2. Find all the places where a string is put inside a string.
+#3. Are you sure there are only four places? How do you know? Maybe I like lying. 
+#4. Explain why adding the two strings w and e with + makes a longer string.
