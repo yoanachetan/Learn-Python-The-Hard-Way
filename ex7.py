4f5675723b7704dbe301f2db60fabb09335698d6
yoanachetan <yoana.chetan@gmail.com>
2019-07-31 06:12:38 +0200

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,29 @@
+print("Marry had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("."* 10) #what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ''at the end. try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end='')
+print(end7 + end8 + end9 + end10 + end11 + end12) 
+
+#Study Drills
+#For these next few exercises, you will have the exact same Study Drills.
+#1. Go back through and write a comment on what each line does.
+#2. Read each one backward or out loud to find your errors.
+#3. From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made
+#4. When you go to the next exercise,look at the mistakes you have made and try not to make them in this new one.
+#5. Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it’s all an act. They make mistakes all the time.
