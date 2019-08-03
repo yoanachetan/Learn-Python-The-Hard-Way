44394365bed5954c7989b02df2188a7e766cbfbb
yoanachetan <yoana.chetan@gmail.com>
2019-08-03 17:23:45 +0200

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,17 @@
+# Here's some new strange stuff, remeber type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun" # Set a variable called days  to a string 
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # Set a variable called months to a string
+
+print("Here are the days:", days) # Print a string, along with days
+print("Here are the months:", months) # Print a string along with months
+
+print("""
+There's something going on here.
+Whith the three double-quptes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""") # print a string with line breaks
+
+# Study Drills
+# Repeat the Study Drill from Exercise 7.
