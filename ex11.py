d73b359dadd72be626fe6f50487a2e6e2f7fa52b
yoanachetan <yoana.chetan@gmail.com>
2019-08-08 14:10:33 +0200

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,14 @@
+print("How old are you?", end='')
+age = input()
+print("How tall are you?", end='')
+height = input()
+print("How much do you weight?", end='')
+weight = input()
+
+print(f"So you're {age} old, {height} tall and {weight} heavy.")
+
+
+#Study Drills
+#1. Go on line and find out what Python’s input does.
+#2. Can you find other ways to use it? Try some of the samples you find. 
+#3. Write another ”form” like this to ask some other questions.
