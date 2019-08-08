bc7bc998a845ee95d23253539764566066c4d2aa
yoanachetan <yoana.chetan@gmail.com>
2019-08-08 14:08:43 +0200

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,21 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+
+#Study Drills
+#1. Memorize all the escape sequences by putting them on flash cards.
+#2. Use'''(triple-single-quote)instead.Canyouseewhyyoumightusethatinsteadof"""?  
+#3. Combine escape sequences and format strings to create a more complex format.
