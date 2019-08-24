9daf33680ae5a98f52523c6e21f80d5e7c553f0d
yoanachetan <yoana.chetan@gmail.com>
2019-08-24 07:43:12 +0200

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,13 @@
+ge = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weight?")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+#Study Drills
+#1. In Terminal, where you normally run python3.6 to run your scripts, type pydoc input. Read
+what it says. If you’re on Windows try python3.6 -m pydoc input instead. 
+#2. Get out of py doc by typingq to quit.
+#3. Look online for what the py doc command does.
+#4. Use py doc to also read about open,file,os,andsys.It’s alright if you do not understandthose;just read through and take notes about interesting things.
